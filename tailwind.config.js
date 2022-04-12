18e072c21058565510bd5960e6b98f4a23473c12
Nizar Baihaqi <nizar.albaihaqi252@gmail.com>
2022-04-12 11:46:15 +0700

    base heading styles 🎨

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
 module.exports = {
-  content: [],
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
+    fontSize: {
+      'sm': '0.9rem',
+      'base': '1rem',
+      'md': '1.5rem',
+      'lg': '2rem',
+      'xl': '2.5rem',
+      '2xl': '3rem',
+      '3xl': '4rem'
+    },
     extend: {},
   },
   plugins: [],
